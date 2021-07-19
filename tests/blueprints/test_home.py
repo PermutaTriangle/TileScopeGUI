62e0dd48d059e9db3d88b9f2103a6163b067e9b0
JonSteinn <jonsteinn@gmail.com>
2021-07-19 18:00:49 +0000

    mock paths, again 2

--- tests/blueprints/test_home.py
+++ tests/blueprints/test_home.py
@@ -8,7 +8,9 @@ def test_app():
     with MockPaths():
         from tilescopegui.factory import TestingConfig, create_app
 
-        yield create_app(TestingConfig())
+        app = create_app(TestingConfig())
+        app.blueprints["home_blueprint"].template_folder = MockPaths._TMP.as_posix()
+        yield app
 
 
 @pytest.fixture
